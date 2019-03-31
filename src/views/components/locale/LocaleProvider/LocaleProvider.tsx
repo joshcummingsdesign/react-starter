dfcfb274e89c40fad21e1b98caca701eea5ac816
josh <hello@joshcummingsdesign.com>
2019-03-30 17:36:18 -0700

    Adds locale via loop

--- src/views/components/locale/LocaleProvider/LocaleProvider.tsx
+++ src/views/components/locale/LocaleProvider/LocaleProvider.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import { ReduxComponent } from '@state/types/redux';
-import { IntlProvider, addLocaleData } from 'react-intl';
+import { IntlProvider, addLocaleData, LocaleData } from 'react-intl';
 import { connect } from 'react-redux';
 import { RootState } from '@src/state/root';
-import localeData from '@src/locale/localeData/data.json';
-import en from 'react-intl/locale-data/en';
-import es from 'react-intl/locale-data/es';
-
-addLocaleData([...en, ...es]);
+import languages from '@src/locale/localeData/data.json';
 
 interface StateProps {
   lang: string;
 }
 
+export enum Languages {
+  en = 'en',
+  es = 'es'
+}
+
+// Add localeData
+const localeData: LocaleData = [];
+Object.keys(Languages).forEach(lang => {
+  console.log(lang);
+  localeData.push(...require(`react-intl/locale-data/${lang}`));
+});
+addLocaleData(localeData);
+
 class LocaleProvider extends ReduxComponent<{}, StateProps> {
   render() {
     const { lang, children } = this.props;
     return (
-      <IntlProvider locale={lang} messages={(localeData as any)[lang]}>
+      <IntlProvider locale={lang} messages={(languages as any)[lang]}>
         {children}
       </IntlProvider>
     );
